21bbefc8866c513fa51739f24b690d763b499937
EmilEng99 <147980819+EmilEng99@users.noreply.github.com>
2024-01-11 19:58:48 +0100

    New version

--- Individuell inlämningsuppgift - Emil Englesson/Individuell-inlämningsuppgift.cs
+++ Individuell inlämningsuppgift - Emil Englesson/Individuell-inlämningsuppgift.cs
@@ -2,150 +2,217 @@
 
 class Program
 {
+    static List<string> history = new List<string>(); //Lista för resultaten i historiken
+
     static void Main(string[] args)
     {
-        bool result = false;
-        bool userInput = false;
+        string? userInput1 = " ";
+        string? userInput2 = " ";
+        string? userInputOperator = " ";
+        decimal result = 0;
+        bool startCalc = false;
+        bool restart = false;
 
         Console.WriteLine("Miniräknare");
         Console.WriteLine("--------------------------");
         Console.WriteLine("");
         Console.WriteLine("Välkommen till denna miniräknare gjord av Emil Englesson.");
-        
-        while(result == false)
+
+        do
         {
-            string userInput1 = " ";
-            string userInputOperator = " ";
-            string userInput2 = " ";
-            int userResult = 0;
-            bool operatorInput = false;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Skriv 's' för att starta, 'h' för historiken, eller 'a' för att avsluta programmet");
+                string? startInput = Console.ReadLine();
+
+            if (startInput?.ToLower() == "h")
+            {
+                ShowHistory();
+            }
+            else if (startInput?.ToLower() == "s")
+            {
+                startCalc = true;
+            }
+            else if (startInput?.ToLower() == "a")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Inte ett giltigt val, vänligen försök igen.");
+                Console.WriteLine("");
+            }
+
+            } while (startCalc == false);
+                    
+            do
+            {
+                Console.WriteLine("Skriv in ditt första tal, och tryck ENTER:");
+                userInput1 = Console.ReadLine();
+
+            } while (UserInputs(userInput1) == false);
+
+            UserInputs(userInput1);
 
             do
             {
-                Console.WriteLine("Skriv in ditt första tal:");
-                userInput1 = (Console.ReadLine());
-
-                if (userInput1 == "" || !int.TryParse(userInput1, out int notValid1)){ 
-                    Console.WriteLine("Du behöver skriva in ett tal för att starta, försök igen.");
-                    userInput = false; 
-                } 
-                else
-                {
-                    userInput = true;
-                }
-            //Kontrollerar att det första talet är ett nummer
-
-                if (userInput1 != null){
-                do
-                {
-                    Console.WriteLine("");
-                    Console.WriteLine("Skriv in tecknet för vilket alternativ du vill använda:");
-                    Console.WriteLine("+  Addera");
-                    Console.WriteLine("-  Subtrahera");
-                    Console.WriteLine("*  Multiplicera");
-                    Console.WriteLine("/  Dividera");
-                
-                    userInputOperator = Console.ReadLine();
-
-                    if (userInputOperator == "+" || userInputOperator == "-" || userInputOperator == "*" || userInputOperator == "/") // fungerar inte
-                    {
-                        operatorInput = true;
-                        userInput = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Inmatningen var inte ett tecken, försök igen.");
-                    }
-                } while (operatorInput == false);
-                //Kontrollerar att ett tecken skrivs in
-                
-                do
-                {
-                    Console.WriteLine("Skriv in ditt andra tal:");
-                    userInput2 = (Console.ReadLine());
-
-                    if (userInput2 == "" || !int.TryParse(userInput2, out int notValid2)){ 
-                        Console.WriteLine("Du behöver skriva in ett tal för att fortsätta, försök igen."); 
-                        userInput = false;
-                    } 
-                    if (userInputOperator == "/")
-                    {
-                        if (int.TryParse(userInput2, out int divideZero))
-                        {
-                            if (divideZero == 0)
-                            {
-                                userInput = false;
-                                Console.WriteLine("");
-                                Console.WriteLine("Ogiltig inmatning! Skriv in ett annat värde");
-                                //Ifall andra talet är lika med 0
-                            }
-                            else
-                            {
-                                userInput = true;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        userInput = true;
-                    }
-
-                int inputNumber1 = int.Parse(userInput1);
-                int inputNumber2 = int.Parse(userInput2);
-                //Omvandlar userInput från string till int för matematiska beräkningar
-
-                
-                if (userInputOperator == "+")
-                {
-                    userResult = inputNumber1 + inputNumber2;
-                }
-                else if (userInputOperator == "-")  
-                {
-                    userResult = inputNumber1 - inputNumber2;
-                }
-                else if (userInputOperator == "*")  
-                {
-                    userResult = inputNumber1 * inputNumber2;
-                }
-                else if (userInputOperator == "/")  
-                {
-                    userResult = inputNumber1 / inputNumber2;
-                    
-                }
-
                 Console.WriteLine("");
-                Console.WriteLine("Svar:");
-                Console.WriteLine($"{inputNumber1} {userInputOperator} {inputNumber2} = {userResult}");
-                Console.WriteLine("");
+                Console.WriteLine("Skriv in tecknet för vilket alternativ du vill använda, och tryck ENTER:");
+                Console.WriteLine("+  Addera");
+                Console.WriteLine("-  Subtrahera");
+                Console.WriteLine("*  Multiplicera");
+                Console.WriteLine("/  Dividera");
+                userInputOperator = Console.ReadLine();
 
-                } while(userInput == false);
-                }
-                 
+            } while (UserInputOperator(userInputOperator) == false);
+
+            UserInputOperator(userInputOperator);
             
-                Console.WriteLine("Skriv \"s\" för att starta om, eller \"a\" för att avsluta.");
-                string endChoice = Console.ReadLine();
-                Console.WriteLine("");
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Skriv in ditt andra tal, och tryck ENTER:");
+                    userInput2 = Console.ReadLine();
+                } while (UserInputs(userInput2) == false);
+                
+                UserInputs(userInput2); //Kontrollerar först att det är ett tal
+                
+            } while (DivideByZero(userInputOperator, userInput2) == false); //Kontrollerar ifall "0" blivit inskrivet och "/" valdes i föregående steg
 
-                if (endChoice == "a" || endChoice == "A")
-                {
-                    result = true; // Avslutar programmet
-                }
-                else if (endChoice == "s" || endChoice == "S")
-                {
-                    result = false; // Startar om programmet
-                }
-            }while(userInput == false);
-        }
+            
+
+            decimal inputNumber1 = Convert.ToDecimal(userInput1);
+            decimal inputNumber2 = Convert.ToDecimal(userInput2);
+
+            result = InputCalculations(inputNumber1, userInputOperator, inputNumber2);
+            
+
+            string endResult = $"{userInput1} {userInputOperator} {userInput2} = " + result;
+            Console.WriteLine($"{endResult}");
+            Console.WriteLine("");
+            history.Add(endResult); //Lägger till i historiken ifall miniräknaren börjar om
+
+            do
+            {
+            Console.WriteLine("Välj ett av valen:\n 1. Meny(ny uträkning, se historik)\n 2. Avsluta miniräknaren");
+            string? endQuestion = Console.ReadLine();
+
+            if (endQuestion == "1")
+            {
+                startCalc = false;
+                restart = true;
+            }
+            else if (endQuestion == "2")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Inget val gjort");
+            }
+            } while (restart == false);
+
+        } while (startCalc == false);
     }
-}
 
-// Välkomnande meddelande - klar
-// Användaren matar in tal och matematiska operation - klar
-// OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet! - klar
-// Ifall användaren skulle dela med 0 visa Ogiltig inmatning! - klar
-// Fråga användaren om den vill avsluta eller fortsätta. - klar
-// En lista för att spara historik för räkningar
-// Lägga resultat till listan
-// Visa resultat
-// Fråga användaren om den vill visa tidigare resultat.
-// Visa tidigare resultat
+    static bool UserInputs(string? UserInput)
+    {
+        bool userInput = false;
+        do
+        {
+        
+        if (UserInput == "" || !int.TryParse(UserInput, out int notValid1)){ 
+            Console.WriteLine("Du behöver skriva in ett tal för att starta, försök igen.");
+            return false;
+        } 
+        else
+        {
+            userInput = true;
+            return true;
+        }
+        } while (userInput == false);          
+    } //Kontrollerar att det första talet är ett nummer  
+
+    static bool DivideByZero(string? divide, string? zero)
+    {
+    if (divide == "/" && zero == "0")
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Ogiltig inmatning! Skriv in ett annat värde");
+            Console.WriteLine("");
+            return false;
+        }
+        else{
+            return true;
+        }
+    } //Kontrollerar så att det inte går att dividera med 0
+
+    static bool UserInputOperator(string? userInputOperator)
+    {
+        bool operatorInput = false;
+        do
+        {
+
+        if (userInputOperator == "+" || userInputOperator == "-" || userInputOperator == "*" || userInputOperator == "/") 
+            {
+                operatorInput = true;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Inmatningen var inte ett tecken, försök igen.");
+                return false;
+            }
+        } while (operatorInput == false);        
+    } //Kontrollerar så att ett av de giltiga tecknen skrivs in
+
+    static decimal InputCalculations(decimal firstInput, string? Operator, decimal secondInput)
+    {
+        decimal result = 0;
+
+        if (Operator == "+")
+        {
+            result = firstInput + secondInput;
+            return result;
+            
+        }
+        else if (Operator == "-")  
+        {
+            result = firstInput - secondInput;
+            return result;
+        }
+        else if (Operator == "*")  
+        {
+            result = firstInput * secondInput;
+            return result;
+        }
+        else if (Operator == "/")  
+        {
+            result = firstInput / secondInput;
+            return result;
+        }
+        else
+        {
+            return 0;
+        }
+    } //Utför de matematiska operationerna
+
+
+    static void ShowHistory()
+    {
+        if (history.Count > 0)
+        {
+            Console.WriteLine("Historik:");
+            foreach (string prevCalculations in history)
+            {
+                Console.WriteLine(prevCalculations);
+            }
+        } //Skriver ut uträkningarna ifall där finns några, annas visas texten nedan
+        else
+        {
+            Console.WriteLine("Inga uträkningar gjorda");
+        }
+    } 
+}
